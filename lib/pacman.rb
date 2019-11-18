b99875c89729af1ae95c0fc80ff3596d558528f9
deniseh <deniseddi@hotmail.com>
2019-11-18 12:31:08 +1100

    created all moves methods inside pacman.rb

--- lib/pacman.rb
+++ lib/pacman.rb
@@ -8,12 +8,6 @@ class Pacman #PLACE
         @facing = f
     end
 
-    def current_position # REPORT
-        #there should be a method where pacman knows where it is at all times and output to REPORT
-       pacman_position = @x, @y, @facing
-       puts pacman_position.join(' ')
-    end 
-
     # define methods for pacman's actions/ movements
     def move #MOVE
         case @facing
@@ -28,6 +22,36 @@ class Pacman #PLACE
         end
     end
 
+    def left
+        case @facing
+        when 'NORTH'
+          @facing = 'WEST'
+        when 'WEST'
+          @facing = 'SOUTH'
+        when 'SOUTH'
+          @facing = 'EAST'
+        when 'EAST'
+          @facing = 'NORTH'
+        end
+    end
+    
+    def right
+        case @facing
+        when 'NORTH'
+          @facing = 'EAST'
+        when 'EAST'
+          @facing = 'SOUTH'
+        when 'SOUTH'
+          @facing = 'WEST'
+        when 'WEST'
+          @facing = 'NORTH'
+        end
+    end
+
+    def current_position # REPORT - There should be a method where pacman knows where it is at all times and show/output to REPORT
+        pacman_position = @x, @y, @facing
+        pacman_position.join(', ')
+     end
     # def move 
     #     # if robot_placed ( haven't build this method yet...)
     #     case @facing 
