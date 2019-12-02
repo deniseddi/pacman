fbe98638f82b41b41a422e100f95964b2841a9eb
deniseh <deniseddi@hotmail.com>
2019-12-02 12:17:01 +1100

    write tests

--- spec/pacman_test.rb
+++ spec/pacman_test.rb
@@ -5,8 +5,10 @@ class PacmanTest < MiniTest::Test
 
     def test_true_for_valid_f
         pacman = Pacman.new(2,2,"North")
-        expected = pacman.valid_f_position?("")
+        expected = pacman.valid_f_position?("") # needs to figure it out
         assert_equal true, expected 
     end
+
+    # write more tests
 
 end
